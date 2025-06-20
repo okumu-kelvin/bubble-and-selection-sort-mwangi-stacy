186014c438a1a11e8e7e8fd50ce9ec46be436047
Stacy Njoki Mwangi <njoki.mwangi@strathmore.edu>
2025-06-20 12:11:38 +0300

    Implementation of Bubble Sort

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,12 @@
 def bubble_sort(unsorted_list):
-    # TODO: Implement bubble sort
-    pass
+    for i in range (len(unsorted_list)-1,0, -1):
+        for j in range(i):
+            if unsorted_list[j]>unsorted_list[j+1]:
+                temp=unsorted_list[j]
+                unsorted_list[j]=unsorted_list[j+1]
+                unsorted_list[j+1]=temp
+
+unsorted_list=[7,9,3,5,1]
+bubble_sort(unsorted_list),
+
+print("Sorted list: ",unsorted_list)

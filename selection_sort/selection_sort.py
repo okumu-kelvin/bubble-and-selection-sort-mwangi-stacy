982e2326efa88ff9c15359552d443d94505c64f4
Stacy Njoki Mwangi <njoki.mwangi@strathmore.edu>
2025-06-20 12:12:14 +0300

    Implementation of Selection Sort

--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,15 @@
 def selection_sort(arr):
-    # TODO: Implement selection sort
-    pass
+    for i in range (len(arr)):
+        minpos=i
+        for j in range(i+1, len(arr)):
+            if arr[j]<arr[minpos]:
+                minpos=j
+
+        temp=arr[i]
+        arr[i]= arr[minpos]
+        arr [minpos] = temp
+
+        print(arr)
+arr=[32,45,67,89,73,68,38,90,21,44,18]
+selection_sort(arr)
+print("Sorted list:", arr)

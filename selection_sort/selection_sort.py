4bb159d4a1deafa939fe5fe64bb39fd5191476ed
Stacy Njoki Mwangi <njoki.mwangi@strathmore.edu>
2025-07-08 09:28:05 +0300

    Update selection_sort.py

--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -8,6 +8,7 @@ def selection_sort(arr):
         temp=arr[i]
         arr[i]= arr[minpos]
         arr [minpos] = temp
+        return(arr)
 
         print(arr)
 arr=[32,45,67,89,73,68,38,90,21,44,18]
